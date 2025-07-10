66e8f51be5795a6797389016d5a1f8aab4830ff8
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-07-11 01:21:51 +0300

    initial setup for server and connecting localhost database to the server

---
+++ fitness_backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json());
+app.use(cors())
+
+connectDB();
+
+
+app.get('/', async (req,res) => {
+    res.status(200).json({
+        message: "hello selam",
+    });
+    
+})
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`the server is running on http://localhost:${process.env.PORT}`);
+});
